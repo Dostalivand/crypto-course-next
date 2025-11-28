99f3d02bbbc4f81fe1275297322d25eabe1f4400
Hosein-DAV <hoseindost33@gmail.com>
2025-11-28 15:53:06 +0330

    update project

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -60,12 +60,6 @@ function Navbar({ cart, setCart }) {
                         <span className="self-center text-2xl lg:text-2xl md:text-lg Moraba-Medium text-zinc-600 dark:text-gray-200">آکادمی ارزهای دیجیتال</span>
                     </Link>
                     <div className="flex md:order-2 items-center gap-x-2 h-13">
-                        {/* Mobile Search Button */}
-                        {/* <button type="button" data-collapse-toggle="navbar-search" aria-controls="navbar-search" aria-expanded="false" className="md:hidden text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 me-1">
-                            <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
-                            </svg>
-                        </button> */}
                         {/* Theme Toggle Btn */}
                         <div className='hidden md:block'>
                             <div className="switch-theme lg:flex size-13 flex items-center justify-center only-icon rounded-2xl bg-gray-200 text-slate-500 dark:bg-white/5 dark:text-white">
